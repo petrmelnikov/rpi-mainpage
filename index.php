df73c3d7b17028f4be45883b9561a8d77ee68977
petr melnikov <petr.melnikvo@gmail.com>
2020-07-19 01:17:07 +0300

    added git pull and wake up actions

--- index.php
+++ index.php
@@ -9,12 +9,29 @@
 <a href="//pi:8080">qbittorrent</a>
 <a href="//gta.pi/">gta</a>
 
-<pre>
-    <?php
+<?php
+
+$action = !empty($_GET['action']) ? $_GET['action'] : '';
+switch ($action) {
+    case 'git-pull':
+        echo shell_exec('git pull 2>&1');
+        break;
+    case 'wake-up-my-pc':
+        echo shell_exec('etherwake 00:D8:61:9F:EF:C9 2>&1');
+        break;
+    case 'index':
+    default:
+        echo '<pre>';
         echo shell_exec('df -h | grep \'Use\|usb_hdd\'');
         echo "\n";
         echo shell_exec('top -b -n 1 | head -20');
         echo "\n";
         echo shell_exec('/opt/vc/bin/vcgencmd measure_temp');
-    ?>
-</pre>
+        echo '</pre>';
+        break;
+}
+
+?>
+<a href="/?action=git-pull">git pull</a>
+<a href="/?action=wake-up-my-pc">wake up my pc</a>
+
