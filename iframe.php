fad9d0fff22430c7f7e3bcaa98d364cda9b3fa9d
petr melnikov <petr.melnikvo@gmail.com>
2020-07-18 23:56:21 +0300

    initial commit

---
+++ iframe.php
@@ -0,0 +1,9 @@
+<html>
+<body style="margin:0;padding:0">
+<form action="//<?php echo $_SERVER['SERVER_ADDR'] ?>:8080/login" method=post>
+    <input type=hidden name=username value=admin>
+    <input type=hidden name=password value=adminadmin>
+    <input type=submit value="login qbt">
+</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<head>
+    <meta name="referrer" content="no-referrer"/>
+</head>
+<!--
+<iframe src=iframe.php style="border:none;height:30;width:110">
+</iframe>
+--!>
+<a href="/media/">media</a>
+<a href="//<?php echo $_SERVER['SERVER_ADDR'] ?>:8080">qbittorrent</a>
+<a href="/gta/public/index.php">gta</a>
+
+<pre>
+    <?php
+        echo shell_exec('df -h | grep \'Use\|usb_hdd\'');
+        echo "\n";
+        echo shell_exec('top -b -n 1 | head -20');
+        echo "\n";
+        echo shell_exec('/opt/vc/bin/vcgencmd measure_temp');
+    ?>
+</pre>
