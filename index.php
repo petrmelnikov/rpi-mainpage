f3f2bad67f424bc19dbaa28fb5ad648992f78c9a
petr melnikov <petr.melnikvo@gmail.com>
2020-07-19 17:56:58 +0300

    php code and html now in different files

--- index.php
+++ index.php
@@ -1,40 +1,25 @@
-<head>
-    <meta name="referrer" content="no-referrer"/>
-</head>
-<!--
-<iframe src=iframe.php style="border:none;height:30;width:110">
-</iframe>
---!>
-<a href="/">index</a>
-<a href="/media/">media</a>
-<a href="//pi.lan:8080">qbittorrent</a>
-<a href="//gta.pi.lan/">gta</a>
-<a href="//router.lan/">router</a>
-<br>
 <?php
+
+//require_once __DIR__ . '/vendor/autoload.php';
 
 $action = !empty($_GET['action']) ? $_GET['action'] : '';
 switch ($action) {
     case 'git-pull':
-        echo shell_exec('git pull 2>&1');
+        $content = shell_exec('git pull 2>&1');
         break;
     case 'wake-up-my-pc':
-        echo shell_exec('etherwake 00:D8:61:9F:EF:C9 2>&1');
+        $content = shell_exec('etherwake 00:D8:61:9F:EF:C9 2>&1');
         break;
     case 'index':
     default:
-        echo '<pre>';
-        echo shell_exec('df -h | grep \'Use\|usb_hdd\'');
-        echo "\n";
-        echo shell_exec('top -b -n 1 | head -20');
-        echo "\n";
-        echo shell_exec('/opt/vc/bin/vcgencmd measure_temp');
-        echo '</pre>';
+        $content = '<pre>' .
+            shell_exec('df -h | grep \'Use\|usb_hdd\'') .
+            "\n" .
+            shell_exec('top -b -n 1 | head -20') .
+            "\n" .
+            shell_exec('/opt/vc/bin/vcgencmd measure_temp') .
+            '</pre>';
         break;
 }
 
-?>
-<br>
-<a href="/?action=git-pull">git pull</a>
-<a href="/?action=wake-up-my-pc">wake up my pc</a>
-
+require_once 'web/main.html.php';
---
+++ web/main.html.php
@@ -0,0 +1,18 @@
+<head>
+    <meta name="referrer" content="no-referrer"/>
+</head>
+<!--
+<iframe src=iframe.php style="border:none;height:30;width:110">
+</iframe>
+--!>
+<a href="/">index</a>
+<a href="/media/">media</a>
+<a href="//pi.lan:8080">qbittorrent</a>
+<a href="//gta.pi.lan/">gta</a>
+<a href="//router.lan/">router</a>
+<br>
+<?= $content ?>
+<br>
+<a href="/?action=git-pull">git pull</a>
+<a href="/?action=wake-up-my-pc">wake up my pc</a>
+
