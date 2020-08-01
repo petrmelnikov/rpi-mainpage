5b80d4587f4368023db49c055a8e7f4319817da8
petr melnikov <petr.melnikvo@gmail.com>
2020-08-01 18:21:49 +0300

    added mi home link

--- web/main.html.php
+++ web/main.html.php
@@ -17,6 +17,7 @@
                 <a class="btn btn-primary" href="//pi.lan:8080">qbittorrent</a>
                 <a class="btn btn-primary" href="//gta.pi.lan/">gta</a>
                 <a class="btn btn-primary" href="//router.lan/">router</a>
+                <a class="btn btn-primary" href="//mihome.pi.lan/">mi home</a>
             </div>
         </div>
         <div class="row">
