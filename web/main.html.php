65f9b2c15b2cac40832d44f8255561d5b79a682c
petrmelnikov <34571405+petrmelnikov@users.noreply.github.com>
2021-09-01 18:25:01 +0300

    Update main.html.php
    returned extra slash for links

--- web/main.html.php
+++ web/main.html.php
@@ -14,11 +14,11 @@
             <div class="col-sm">
                 <a class="btn btn-primary" href="/">index</a>
                 <a class="btn btn-primary" href="/media/">media</a>
-                <a class="btn btn-primary" href="/pi.lan:8080">qbittorrent</a>
-                <a class="btn btn-primary" href="/gta.pi.lan/">gta</a>
-                <a class="btn btn-primary" href="/router.lan/">router</a>
-                <a class="btn btn-primary" href="/mihome.pi.lan/">mi home</a>
-                <a class="btn btn-primary" href="/printer.lan/">printer</a>
+                <a class="btn btn-primary" href="//pi.lan:8080">qbittorrent</a>
+                <a class="btn btn-primary" href="//gta.pi.lan/">gta</a>
+                <a class="btn btn-primary" href="//router.lan/">router</a>
+                <a class="btn btn-primary" href="//mihome.pi.lan/">mi home</a>
+                <a class="btn btn-primary" href="//printer.lan/">printer</a>
             </div>
         </div>
         <div class="row">
