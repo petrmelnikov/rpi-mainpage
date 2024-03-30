9065b5c5fa10e7a7b9f8588fa29b9e68e5c0193b
petr melnikov <petr.melnikvo@gmail.com>
2024-03-30 20:59:55 +0300

    wip

--- run_server_in_screen.sh
+++ run_server_in_screen.sh
@@ -1,3 +1,3 @@
 #!sh
 screen -d -m -S rpi_mainpage_php_server
-screen -S rpi_mainpage_php_server -X stuff "php -S localhost:8111^M"
+screen -S rpi_mainpage_php_server -X stuff "php -S 0.0.0.0:8111^M"
