a3e89101e683e1d0c12efcb1725843459de87908
petrmelnikov <34571405+petrmelnikov@users.noreply.github.com>
2024-06-19 02:45:56 +0300

    Update top_main_menu.html.php

--- templates/top_main_menu.html.php
+++ templates/top_main_menu.html.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-sm">
-        <a class="btn btn-primary" href='http://192.168.0.164:8080'>qbittorrent</a>
-        <a class="btn btn-primary" href='https://192.168.0.164:8096'>jellyfin</a>
+        <a class="btn btn-primary" href='https://192.168.0.164:8080'>qbittorrent</a>
+        <a class="btn btn-primary" href='http://192.168.0.164:8096'>jellyfin</a>
 <!--        <a class="btn btn-primary" href="/media/">media</a>-->
 <!--        <a class="btn btn-primary" href="//pi.lan:8080">qbittorrent</a>-->
 <!--        <a class="btn btn-primary" href="//gta.pi.lan/">gta</a>-->
