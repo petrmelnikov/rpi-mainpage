32d1a0910ce02e32bee0807c02dd234f9821f62d
Petr Melnikov <petr.melnikov@gmail.com>
2026-01-07 12:16:11 +0200

    fixed git pull

--- app/Controller/SystemController.php
+++ app/Controller/SystemController.php
@@ -7,11 +7,16 @@ use App\ShellCommandExecutor;
 
 class SystemController
 {
+    private string $appRoot = '';
+
     public function registerRoutes(Router $router, string $appRoot): void
     {
+        $this->appRoot = $appRoot;
+
         $router->addRoute('GET', '', [$this, 'index'], $appRoot . '/templates/shell_command_raw_content.html.php');
         $router->addRoute('GET', '/top', [$this, 'top'], $appRoot . '/templates/shell_command_raw_content.html.php');
         $router->addRoute('GET', '/update-code', [$this, 'updateCode'], $appRoot . '/templates/shell_command_raw_content.html.php');
+        $router->addRoute('POST', '/update-code', [$this, 'updateCode'], $appRoot . '/templates/shell_command_raw_content.html.php');
     }
 
     public function index(): array
@@ -30,10 +35,12 @@ class SystemController
 
     public function updateCode(): array
     {
+        $appRootArg = escapeshellarg($this->appRoot !== '' ? $this->appRoot : getcwd());
+
         return ['shellCommandRawContent' => array_merge(
             ShellCommandExecutor::executeWithSplitByLines('ssh -T git@github.com 2>&1'),
-            ShellCommandExecutor::executeWithSplitByLines('git pull 2>&1'),
-            ShellCommandExecutor::executeWithSplitByLines('composer install 2>&1')
+            ShellCommandExecutor::executeWithSplitByLines("git -C {$appRootArg} pull --ff-only 2>&1"),
+            ShellCommandExecutor::executeWithSplitByLines("composer --working-dir={$appRootArg} install 2>&1")
         )];
     }
 }
--- templates/tools.html.php
+++ templates/tools.html.php
@@ -2,5 +2,9 @@
     <div class="col-sm">
         <a class="btn btn-secondary" href="/tools/example1">example1</a>
         <a class="btn btn-secondary" href="/tools/example2">example2</a>
+
+        <form method="post" action="/update-code" style="display:inline-block; margin-left: 8px;">
+            <button class="btn btn-primary" type="submit">git pull</button>
+        </form>
     </div>
 </div>
